869a5988d25c0b1987ceabe0d33e077a94e26343
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-06-14 21:36:30 +0530

    auth sync screen

--- app/src/main/java/nibm/mad/snapshop/MainActivity.kt
+++ app/src/main/java/nibm/mad/snapshop/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import nibm.mad.snapshop.screens.auth.presentation.AuthSyncScreen
 import nibm.mad.snapshop.screens.permissions.presentation.MediaPermissionScreen
 import nibm.mad.snapshop.ui.theme.SnapShopTheme
 
@@ -13,7 +14,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SnapShopTheme {
-                MediaPermissionScreen({})
+                AuthSyncScreen({})
             }
         }
     }
