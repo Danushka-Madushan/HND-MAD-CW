9af02dfc78afe4d6fdf00172aa89a665deb89510
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-06-14 20:16:04 +0530

    roboto font family

--- app/src/main/java/nibm/mad/snapshop/ui/theme/Type.kt
+++ app/src/main/java/nibm/mad/snapshop/ui/theme/Type.kt
@@ -2,33 +2,40 @@ package nibm.mad.snapshop.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import nibm.mad.snapshop.R
 
-// Set of Material typography styles to start with
+val Roboto = FontFamily(
+    Font(resId = R.font.roboto_light, weight = FontWeight.Light),
+    Font(resId = R.font.roboto_regular, weight = FontWeight.Normal),
+    Font(resId = R.font.roboto_medium, weight = FontWeight.Medium),
+    Font(resId = R.font.roboto_semibold, weight = FontWeight.SemiBold),
+    Font(resId = R.font.roboto_bold, weight = FontWeight.Bold),
+)
+
 val Typography = Typography(
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Normal,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = Roboto,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     )
-    */
 )
