050afe02432e2094538d99c0d653e1d4bcb1341c
Danushka-Madushan <danushkamadushan128@gmail.com>
2026-06-18 13:51:24 +0530

    implementations of home screen

--- app/src/main/java/nibm/mad/snapshop/MainActivity.kt
+++ app/src/main/java/nibm/mad/snapshop/MainActivity.kt
@@ -3,18 +3,15 @@ package nibm.mad.snapshop
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import nibm.mad.snapshop.screens.auth.presentation.AuthSyncScreen
-import nibm.mad.snapshop.screens.permissions.presentation.MediaPermissionScreen
+import nibm.mad.snapshop.screens.home.HomeScreen
 import nibm.mad.snapshop.ui.theme.SnapShopTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             SnapShopTheme {
-                AuthSyncScreen({})
+                HomeScreen()
             }
         }
     }
